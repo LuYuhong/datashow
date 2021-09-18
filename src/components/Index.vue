1ac950187804e38adacf5e95fd2f3fc2331ca261
littleloo <36443905+littleloo@users.noreply.github.com>
2021-09-18 10:30:49 +0800

    create

---
+++ src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+      <p>lzx nb 666</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+p{
+    font-size: 25px;
+    text-align: center;
+}
+
+</style>
